64b1aa01d97c37b7621e59c96247ec1fa70ea9ff
Roman <efimchuk.roma@gmail.com>
2014-08-01 02:33:10 +0300

    nope

--- startup_db.bat
+++ startup_db.bat
@@ -1,1 +1,6 @@
+IF exist local_db (
 "C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath "D:\node-authentication\local_db"
+) ELSE ( 
+mkdir local_db 
+"C:\Program Files\MongoDB 2.6 Standard\bin\mongod.exe" --dbpath "D:\node-authentication\local_db"
+)
