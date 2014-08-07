57abf4210c0bc2e064abf068091d516e00adb51a
Roman <efimchuk.roma@gmail.com>
2014-08-08 01:44:22 +0300

    nope

---
+++ db.sql
@@ -0,0 +1,42 @@
+CREATE database plocal:local_db admin admin plocal graph
+
+CREATE CLASS Workspace
+CREATE PROPERTY Workspace.name STRING
+CREATE PROPERTY Workspace.creatorId STRING
+CREATE PROPERTY Workspace.createdDate LONG
+
+CREATE CLASS PermittedWorkspace
+CREATE PROPERTY PermittedWorkspace.userId STRING
+CREATE PROPERTY PermittedWorkspace.workspaceId STRING
+CREATE PROPERTY PermittedWorkspace.isOwn BOOLEAN
+CREATE PROPERTY PermittedWorkspace.isDefault BOOLEAN
+CREATE PROPERTY PermittedWorkspace.readOnly BOOLEAN
+CREATE PROPERTY PermittedWorkspace.collectionManager BOOLEAN
+CREATE PROPERTY PermittedWorkspace.accessManager BOOLEAN
+
+CREATE CLASS User
+CREATE PROPERTY User.accountId STRING 
+CREATE PROPERTY User.currentWorkspaceId STRING
+CREATE PROPERTY User.registeredDate LONG
+
+CREATE CLASS OwnWorkspace
+CREATE PROPERTY OwnWorkspace.userId STRING 
+CREATE PROPERTY OwnWorkspace.workspaceId STRING 
+
+CREATE CLASS Todo
+CREATE PROPERTY Todo.userId STRING
+CREATE PROPERTY Todo.title STRING
+CREATE PROPERTY Todo.completed BOOLEAN
+CREATE PROPERTY Todo.workspaceId STRING
+CREATE PROPERTY Todo.createdDate LONG
+
+CREATE CLASS UserAccount
+CREATE PROPERTY UserAccount.userId STRING
+CREATE PROPERTY UserAccount.genericId STRING
+CREATE PROPERTY UserAccount.displayName STRING
+CREATE PROPERTY UserAccount.password STRING
+CREATE PROPERTY UserAccount.email STRING
+CREATE PROPERTY UserAccount.token STRING
+CREATE PROPERTY UserAccount.authorizationProvider STRING
+CREATE PROPERTY UserAccount.registeredDate LONG
+
