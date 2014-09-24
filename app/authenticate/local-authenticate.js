841a21845cedc3a170686a3c789102673bc1b94f
Roman <efimchuk.roma@gmail.com>
2014-09-24 23:53:41 +0300

    nope

--- app/authenticate/local-authenticate.js
+++ app/authenticate/local-authenticate.js
@@ -1,85 +1,91 @@
 "use strict";
 
-module.exports = function (app, passport, dbProvider, developmentMode) {
+(function (require) {
 
-    var Exception = require('../exception');
+    module.exports = function (app, passport, dbProvider, developmentMode) {
 
-    function getError(error) {
-        if (error instanceof Exception) {
-            return error;
+        var Exception = require('../exception');
+        var JSON = require('json3');
+
+        function getError(error) {
+            if (error instanceof Exception) {
+                return error;
+            }
+            return new Exception(Exception.UNHANDLED_EXCEPTION, 'Unhandled exception', error);
         }
-        return new Exception(Exception.UNHANDLED_EXCEPTION, 'Unhandled exception', error);
-    }
 
-    function send(response, data) {
-        process.nextTick(function () {
-            response.send(JSON.stringify(data));
-        });
-    }
+        function send(response, data) {
+            process.nextTick(function () {
+                var json = JSON.stringify(data);
+                response.send(json);
+            });
+        }
 
-    function getAuthenticateHandler(request, response, callback) {
-        return function (error, user, info) {
-            if (error) {
-                send(response, {
-                    status: false,
-                    error: getError(error)
-                });
-            } else {
-                if (info instanceof Exception) {
+        function getAuthenticateHandler(request, response, callback) {
+            return function (error, user, info) {
+                if (error) {
                     send(response, {
                         status: false,
-                        error: info
+                        error: getError(error)
                     });
                 } else {
-                    if (user) {
-                        request.logIn(user, function (error) {
-                            if (error) {
-                                send(response, {
-                                    status: false,
-                                    error: getError(error)
-                                });
-                            } else {
-                                if (callback) {
-                                    callback(user, function () {
+                    if (info instanceof Exception) {
+                        send(response, {
+                            status: false,
+                            error: info
+                        });
+                    } else {
+                        if (user) {
+                            request.logIn(user, function (error) {
+                                if (error) {
+                                    send(response, {
+                                        status: false,
+                                        error: getError(error)
+                                    });
+                                } else {
+                                    if (callback) {
+                                        callback(user, function () {
+                                            send(response, {
+                                                status: true
+                                            });
+                                        });
+                                    } else {
                                         send(response, {
                                             status: true
                                         });
-                                    });
-                                } else {
-                                    send(response, {
-                                        status: true
-                                    });
+                                    }
                                 }
-                            }
-                        });
-                    } else {
-                        send(response, {
-                            status: false,
-                            error: new Exception(Exception.USER_NOT_FOUND, 'User not found.')
-                        });
+                            });
+                        } else {
+                            send(response, {
+                                status: false,
+                                error: new Exception(Exception.USER_NOT_FOUND, 'User not found.')
+                            });
+                        }
                     }
                 }
-            }
-        };
-    }
+            };
+        }
 
-    app.post('/api/authenticate/login', function (request, response, next) {
+        app.post('/api/authenticate/login', function (request, response, next) {
 
-        var authenticateHandler = getAuthenticateHandler(request, response, function (user, callback) {
+            var authenticateHandler = getAuthenticateHandler(request, response, function (user, callback) {
 
-            var workspaceId = request.body['workspaceId'];
-            var rootWorkspaceId = request.body['rootWorkspaceId'];
+                var workspaceId = request.body['workspaceId'];
+                var rootWorkspaceId = request.body['rootWorkspaceId'];
 
-            dbProvider.setUserWorkspaceId(user.userId, workspaceId, rootWorkspaceId, callback)
+                dbProvider.setUserWorkspaceId(user.userId, workspaceId, rootWorkspaceId, callback)
+            });
+
+            passport.authenticate('local-login', authenticateHandler)(request, response, next);
         });
 
-        passport.authenticate('local-login', authenticateHandler)(request, response, next);
-    });
+        app.post('/api/authenticate/sign-up', function (request, response, next) {
 
-    app.post('/api/authenticate/sign-up', function (request, response, next) {
+            var authenticateHandler = getAuthenticateHandler(request, response);
 
-        var authenticateHandler = getAuthenticateHandler(request, response);
+            passport.authenticate('local-sign-up', authenticateHandler)(request, response, next);
+        });
+    };
 
-        passport.authenticate('local-sign-up', authenticateHandler)(request, response, next);
-    });
-};
+})(require);
--- app/providers/services-provider.js
+++ app/providers/services-provider.js
@@ -1,93 +1,99 @@
 "use strict";
 
-module.exports = function (app, developmentMode) {
+(function (require) {
 
-    var Exception = require('../exception');
+    module.exports = function (app, developmentMode) {
 
-    function send(request, response, data) {
-        process.nextTick(function () {
-            response.send(JSON.stringify(data));
-        });
-    }
+        var Exception = require('../exception');
+        var JSON = require('json3');
 
-    function handleResult(request, response, result) {
-        if (result) {
-            if (typeof result == 'string') {
+        function send(request, response, data) {
+            process.nextTick(function () {
+                var json = JSON.stringify(data);
+                response.send(json);
+            });
+        }
+
+        function handleResult(request, response, result) {
+            if (result) {
+                if (typeof result == 'string') {
+                    send(request, response, {
+                        status: true,
+                        message: result
+                    });
+                } else {
+                    var message = result.message;
+                    var data = result.data;
+                    if (data) {
+                        send(request, response, {
+                            status: true,
+                            message: message,
+                            data: data
+                        });
+                    } else {
+                        send(request, response, {
+                            status: true,
+                            message: message
+                        });
+                    }
+                }
+            } else {
                 send(request, response, {
-                    status: true,
-                    message: result
+                    status: true
                 });
-            } else {
-                var message = result.message;
-                var data = result.data;
-                if (data) {
-                    send(request, response, {
-                        status: true,
-                        message: message,
-                        data: data
-                    });
-                } else {
-                    send(request, response, {
-                        status: true,
-                        message: message
-                    });
-                }
-            }
-        } else {
-            send(request, response, {
-                status: true
-            });
-        }
-    }
-
-    function executeService(serviceBody, failureCallback) {
-        try {
-            serviceBody();
-        } catch (e) {
-            if (e) {
-                failureCallback(e);
-            } else {
-                e = new Exception(Exception.UNHANDLED_EXCEPTION, 'Unhandled exception');
-                failureCallback(e);
             }
         }
-    }
 
-    function getRequestHandler(callback) {
-        return function (request, response) {
-            executeService(function () {
-                callback(request, response, function (result) {
-                    handleResult(request, response, result);
-                });
-            }, function (e) {
-                if (e instanceof Exception) {
-                    send(request, response, {
-                        status: false,
-                        error: e
-                    });
+        function executeService(serviceBody, failureCallback) {
+            try {
+                serviceBody();
+            } catch (e) {
+                if (e) {
+                    failureCallback(e);
                 } else {
-                    e = new Exception(Exception.UNHANDLED_EXCEPTION, 'Unhandled exception', e);
-                    send(request, response, {
-                        status: false,
-                        error: e
-                    });
+                    e = new Exception(Exception.UNHANDLED_EXCEPTION, 'Unhandled exception');
+                    failureCallback(e);
                 }
-            });
+            }
+        }
+
+        function getRequestHandler(callback) {
+            return function (request, response) {
+                executeService(function () {
+                    callback(request, response, function (result) {
+                        handleResult(request, response, result);
+                    });
+                }, function (e) {
+                    if (e instanceof Exception) {
+                        send(request, response, {
+                            status: false,
+                            error: e
+                        });
+                    } else {
+                        e = new Exception(Exception.UNHANDLED_EXCEPTION, 'Unhandled exception', e);
+                        send(request, response, {
+                            status: false,
+                            error: e
+                        });
+                    }
+                });
+            };
+        }
+
+        return {
+            'get': function (path, callback) {
+                app.get(path, getRequestHandler(callback));
+            },
+            'post': function (path, callback) {
+                app.post(path, getRequestHandler(callback));
+            },
+            'put': function (path, callback) {
+                app.put(path, getRequestHandler(callback));
+            },
+            'delete': function (path, callback) {
+                app.delete(path, getRequestHandler(callback));
+            }
         };
-    }
-
-    return {
-        'get': function (path, callback) {
-            app.get(path, getRequestHandler(callback));
-        },
-        'post': function (path, callback) {
-            app.post(path, getRequestHandler(callback));
-        },
-        'put': function (path, callback) {
-            app.put(path, getRequestHandler(callback));
-        },
-        'delete': function (path, callback) {
-            app.delete(path, getRequestHandler(callback));
-        }
     };
-};
+
+})(require);
